d6f52556bdefa39076a4a04be48b9120b43e75ff
Makoski Víctor <makoski.ed@gmail.com>
2022-08-31 13:46:12 -0300

    NavBar

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,7 +1,42 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import favicon from "../../assets/favicon.ico"
+import { GrCart } from "react-icons/gr";
 
 export default function NavBar () {
     return(
-        <div>NavBar</div>
+        <div>
+            <div>
+                <Link to={"/home"}>
+                    <img src={favicon} alt="Not Found" />
+                </Link>
+            </div>
+            <div>
+                <Link to={"/products"}>
+                    <button>PRODUCTS</button>
+                </Link>
+            </div>
+            <div>
+                <Link to={"/about"}>
+                    <button>ABOUT</button>
+                </Link>
+            </div>
+            <div>
+                <Link to={"/contact"}>
+                    <button>CONTACT</button>
+                </Link>
+            </div>
+            <div>
+                <Link to={"/cart"}>
+                    <GrCart/>
+                </Link>
+            </div>
+            <div>
+                <Link to={"/login"}>
+                    <button>LOGIN</button>
+                </Link>
+            </div>
+
+        </div>
     )
 };
