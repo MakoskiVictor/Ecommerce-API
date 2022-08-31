b1c751002185321e90bed791d7004fc7dd7bd66c
sebastian prado <sebaslkjh@gmail.com>
2022-08-31 13:39:31 -0500

    reorganizacion carpetas About

---
+++ client/src/components/About/About.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import style from "./About.module.css"
+
+
+export default function About() {
+    return (
+        <div>
+            <main>
+                <div className={style.ContainerAbout}>
+                    <section>
+                        <img src="https://images.pexels.com/photos/3107156/pexels-photo-3107156.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="Image About" className={style.imagePrincipal} />
+                        <h2 className={style.aboutTitle}>Who we are?</h2>
+                        <p className={style.aboutInfo}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae nihil quos, voluptate nobis veritatis porro atque officia ducimus suscipit in voluptatibus reprehenderit nesciunt omnis harum praesentium commodi eos blanditiis facere.</p>
+                    </section>
+                    <section className={style.ContainerImagesAbout}>
+                        <img src="https://images.pexels.com/photos/9393441/pexels-photo-9393441.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="Image 1" className={style.imageContain} />
+                        <img src="https://images.pexels.com/photos/13329172/pexels-photo-13329172.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="Image 2" className={style.imageContain} />
+                        <img src="https://images.pexels.com/photos/7585590/pexels-photo-7585590.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="Image 3" className={style.imageContain} />
+                    </section>
+                </div>
+            </main>
+        </div>
+    )
+}
