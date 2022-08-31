aa83c1ceb910ed5b2859049fd9c894b4c354a6dc
Enzo Holgado <enzoholgadodev@gmail.com>
2022-08-31 11:36:26 -0500

    creando actions y reducers

---
+++ client/src/redux/reducers/index.js
@@ -0,0 +1,10 @@
+const initialState = {}
+
+const rootReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case "value":
+    
+        default:
+            return state
+    }
+}
