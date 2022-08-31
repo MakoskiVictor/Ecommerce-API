9ed5c7fc9197bca1791b8309c3147f85437f7410
Enzo Holgado <enzoholgadodev@gmail.com>
2022-08-31 11:47:11 -0500

    cambios en redux e instalacion de dependencias

--- client/src/redux/reducers/index.js
+++ client/src/redux/reducers/index.js
@@ -3,8 +3,10 @@ const initialState = {}
 const rootReducer = (state = initialState, action) => {
     switch (action.type) {
         case "value":
-    
+            break
         default:
             return state
     }
 }
+
+export default rootReducer
