e8d97f8ffe638eb506fa04a529da5817452c4e4d
Diego Cuevas <ingdcuevas@gmail.com>
2022-09-02 16:27:54 -0500

    componente Details - estilos agregados

--- client/src/components/Details/Details.jsx
+++ client/src/components/Details/Details.jsx
@@ -1,53 +1,58 @@
 import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
+import style from "./Details.module.css";
 
 import { deleteDetails, searchProductId } from "../../redux/actions";
 
 export default function Details(props) {
-    console.log(props)
+  console.log(props);
 
-    const dispatch = useDispatch();
-    const detail = useSelector((state) => state.details)
+  const dispatch = useDispatch();
+  const detail = useSelector((state) => state.details);
 
-    useEffect(() => {
-        dispatch(searchProductId(props.match.params.id))
-        dispatch(deleteDetails())
-    }, [dispatch]);
+  useEffect(() => {
+    dispatch(searchProductId(props.match.params.id));
+    dispatch(deleteDetails());
+  }, [dispatch]);
 
-    console.log(detail)
-    console.log(detail.brand)
+  console.log(detail);
+  console.log(detail.brand);
 
-    return (
-        <div>
-            <div>
-                {detail.length > 0 ?
-                    <div>
-                        <h1>{detail[0].name}</h1>
-                        <img src={`https://${detail[0].image}`} alt="Not Found" />
-                        <p>Brand: {detail[0].brand} </p>
-                        <p>Price: ${detail[0].price} </p>
-                        <p>Genre: {detail[0].gender} </p>
-                        <p>Category: {detail[0].category.name}</p>
+  return (
+    <div>
+      <div className={style.cardDetailContainer}>
+        {detail.length > 0 ? (
+          <div>
+            <h1>{detail[0].name}</h1>
+            <div className={style.detailsContainer}>
+              <div className={style.imageContainer}>
+                <img src={`https://${detail[0].image}`} alt="Not Found" />
+              </div>
+              <div className={style.infoContainer}>
+                <p>Brand: {detail[0].brand} </p>
+                <p>Price: ${detail[0].price} </p>
+                <p>Genre: {detail[0].gender} </p>
+                <p>Category: {detail[0].category.name}</p>
+              </div>
+            </div>
 
-                        {/* esta informacion no estan en la base de datos interna:
+            {/* esta informacion no estan en la base de datos interna:
 
                         <p>Type: {detail.type} </p>
                         <p>aboutMe: {`${detail.info.aboutMe}`} </p>
                         <p>careInfo: {`${detail.info.careInfo}`} </p>
                         <p>sizeAndFit: {`${detail.info.sizeAndFit}`} </p> */}
-
-                    </div>
-                    :
-                    <p>LOADING...</p>
-                }
-            </div>
-            <div>
-                <Link to="/rutaPrueba">
-                    <button>Go Back</button>
-                </Link>
-            </div>
-        </div>
-    )
+          </div>
+        ) : (
+          <p>LOADING...</p>
+        )}
+      </div>
+      <div>
+        <Link to="/rutaPrueba">
+          <button className={style.btnDetails}>Go Back</button>
+        </Link>
+      </div>
+    </div>
+  );
 }
-
