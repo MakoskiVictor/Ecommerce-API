c2369cf62b8c06c0b93133c010fe4b1065f4ebde
sebastian prado <sebaslkjh@gmail.com>
2022-09-02 13:09:58 -0500

    creacion de componente Formulario

---
+++ client/src/components/Formulario/Formulario.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import style from "./Formulario.module.css"
+
+function Formulario() {
+    return (
+        <div>Formulario</div>
+    )
+}
+
+export default Formulario
