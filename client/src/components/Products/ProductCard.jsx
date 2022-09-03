d666e4f886c5f3c2d62b619fedb1daeaaf12e3b2
Diego Cuevas <ingdcuevas@gmail.com>
2022-09-03 15:16:28 -0500

    Componentes Details-Paginated-ProductCard-ProductCards ahora cuentan con estilos definidos de acuerdo al diseño que se esta manejando

--- client/src/components/Products/ProductCard.jsx
+++ client/src/components/Products/ProductCard.jsx
@@ -1,24 +1,23 @@
 import React from "react";
 import styles from "./ProductCard.module.css";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-function ProductCard({img,name,brand,price,id}) {
+function ProductCard({ img, name, brand, price, id }) {
   return (
     <div className={styles.productContainer}>
       <div className={styles.photoContainer}>
         <div>
-        <img src={`https://${img}`}  width="140" height="150" alt="" /></div>
+          <img src={`https://${img}`} height="200" alt="" />
+        </div>
       </div>
       <h3>{name}</h3>
       <p>{brand}</p>
       <div className={styles.priceCartContainer}>
         <div>{`$/${price}.00`}</div>
         <Link to={`/details/${id}`}>
-          <button className="botonDetalle">Details</button>
+          <button>Details</button>
         </Link>
       </div>
-
-
     </div>
   );
 }
