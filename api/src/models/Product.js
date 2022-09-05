4bde793b31e4558dd2d55a81a3aeacfbd08536e4
Enzo Holgado <enzoholgadodev@gmail.com>
2022-09-05 17:28:06 -0500

    agregando propiedades Stock y Size al modelo de productos

--- api/src/models/Product.js
+++ api/src/models/Product.js
@@ -1,35 +1,47 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('product', {
-    id:{
-      type:DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey:true,
+  sequelize.define(
+    "product",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      price: {
+        type: DataTypes.DOUBLE,
+        allowNull: false,
+      },
+      image: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      brand: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      gender: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      stock: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      size: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
     },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    price:{
-      type:DataTypes.DOUBLE,
-      allowNull:false,
-    },
-    image:{
-      type:DataTypes.STRING,
-      allowNull:false,
-    },
-    brand:{
-      type:DataTypes.STRING,
-      allowNull:false,
-    },
-    gender:{
-      type:DataTypes.STRING,
-      allowNull:false,
+    {
+      timestamps: false,
     }
-  },{
-    timestamps: false,
-  });
+  );
 };
