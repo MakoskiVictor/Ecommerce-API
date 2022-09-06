ba30abe9db6cf5c5fa76870355d994f58960d1ec
Angelo <rideralucar@gmail.com>
2022-09-06 10:11:16 -0500

    base para el carry

---
+++ client/src/components/Carry/Carry.jsx
@@ -0,0 +1,28 @@
+/*import React, { Component } from "react";
+import { connect } from "react-redux";
+import { changePaginatedPage } from "../../redux/actions";
+import style from "./Paginated.module.css";
+
+class Carry extends Component {
+  render() {
+   console.log(this.props.carryProducts);
+   return(
+    HOLA
+   );
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    carryProducts: state.carryProducts,
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  //pasandole al componente la posibilidad como props de hacer un dispatch de la function getcountries
+  return {
+    //changePaginatedPage: (page) => dispatch(changePaginatedPage(page)),
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Carry);*/
