3ae5ef7e8a0a0aaf9d5703d08871ffaee52ce567
Marmannn <melfy15@hotmail.com>
2022-09-08 19:39:40 -0300

    actualizacion en Navbar

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -25,69 +25,75 @@ export default function NavBar() {
     return (
         <header>
             <div>
-                <nav className={Style.NavBarComplete}>
-                    <div className={Style.left}>
-                        <Link to={"/"}>
-                            {/* <img src={logo_wooly} alt="Not Found" width="85px" height="85px" className={Style.logo} /> */}
-                            <img src={logo} alt="Not Found" width="85px" height="85px" className={Style.logo} />
-                        </Link>
-                    </div>
-                    <div className={Style.center}>
-                        <div className={Style.container}>
-                            <ul className={Style.NavUl}>
+        <nav className={Style.NavBarComplete}>
+            <div className={Style.left}>
+                <Link to={"/"}>
+                    {/* <img src={logo_wooly} alt="Not Found" width="85px" height="85px" className={Style.logo} /> */}
+                    <img src={logo} alt="Not Found" width="85px" height="85px" className={Style.logo} />
+                </Link>
+            </div>
+            <div className={Style.center}>
+                <div className={Style.container}>
+                    <ul className={Style.NavUl}>
 
-                                {/* <Link to={"/products"}>
+                    {/* <Link to={"/products"}>
                         PRODUCTS
                     </Link> */}
-                                <li>
-                                    <Link to={"/products/Men"} className={Style.letra}>
-                                        MEN
-                                    </Link>
-                                    <Link to={"/products/Women"} className={Style.letra}>
-                                        WOMEN
-                                    </Link>
-                                </li>
-                                <li>
+                    <li>
+                        <Link to={"/products/Men"} className={Style.letra}>
+                            MEN
+                        </Link>
+                        <Link to={"/products/Women"} className={Style.letra}>
+                            WOMEN
+                        </Link>
+                    </li>
+                    <li>
 
-                                    <Link to={"/about"} className={Style.letra}>
-                                        ABOUT
-                                    </Link>
+                    <Link to={"/about"} className={Style.letra}>
+                        ABOUT
+                    </Link>
 
-                                    <Link to={"/contact"} className={Style.letra}>
-                                        CONTACT
-                                    </Link>
+                    <Link to={"/contact"} className={Style.letra}>
+                        CONTACT
+                    </Link>
 
-                                    <Link to={"/carry"} className={Style.letra}>
-                                        CARRY
-                                    </Link>
-                                    {/*<Link to={"/about"} className={Style.letra}>
+                    <Link to={"/carry"} className={Style.letra}>
+                         CARRY
+                    </Link>
+                    {/*<Link to={"/about"} className={Style.letra}>
                         LOGIN
                     </Link>
                     <Link to={"/register"} className={Style.letra}>
                         REGISTER
                     </Link>*/}
-                                    <Link to={"/createProduct"} className={Style.letra}>
-                                        CREATE_PRODUCT
-                                    </Link>
-                                    <div>
-                                        <button onClick={handleOpen} className='button_login'>Inicia sesión</button>
-                                    </div>
-                                    <div>
-                                        <Link to='/register'><button className='button_register'>Regístrate</button></Link>
-                                    </div>
-                                </li>
-                            </ul>
+                    <Link to={"/createProduct"} className={Style.letra}>
+                        CREATE_PRODUCT
+                    </Link>
 
-                        </div>
-                    </div>
-                    {/* <div className={Style.right}>
+                    
+                    </li>
+                    
+                    
+                        
+                  <button onClick={handleOpen} className={Style.buttonlogin}>Inicia sesión</button>
+                       
+                     
+                  <Link to='/register'><button className={Style.buttonregister}>Regístrate</button></Link>
+                
+                                         
+                          
+                </ul>
+
+                </div>
+            </div>
+            {/* <div className={Style.right}>
                 <Link to={"/create"}>
                     ADD CLOTHES
                 </Link>
             </div> */}
 
-                </nav>
-            </div>
+        </nav>       
+        </div>
             {openModal &&
                 <div className={Style.ModalAbiertoBackground}></div>
             }
