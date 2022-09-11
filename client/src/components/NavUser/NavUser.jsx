10fe7299756140c4ce76739cec004fbc7e1d225c
sebastian prado <sebaslkjh@gmail.com>
2022-09-11 17:58:54 -0500

    Funcionalidad Logout Google  y email

--- client/src/components/NavUser/NavUser.jsx
+++ client/src/components/NavUser/NavUser.jsx
@@ -1,30 +1,45 @@
 import { BiUser } from "react-icons/bi";
 import { IconContext } from "react-icons";
 import Style from "./NavUser.module.css"
-import { useDispatch} from "react-redux";
-import {Logout} from "../../redux/actions";
+import { useDispatch } from "react-redux";
+import { Logout } from "../../redux/actions";
+import { useAuth } from "../../context/authContext";
+
 
 function NavUser() {
+  //login Google
+  const { logout, user } = useAuth();
 
+  const handleLogout = async () => {
+    try {
+      await logout();
+      dispatch(Logout());
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
+  // login email
   const dispatch = useDispatch()
-  
- function OnLogout(){
-  dispatch(Logout());
-}
+
+  // function OnLogout() {
+
+  // }
 
 
   return (
     <div class={Style.navbar}>
       <div class={Style.dropdown}>
-      
+
         <button class={Style.dropbtn} >
-          <IconContext.Provider  value={{ color: 'white', size: '25px'}}>
-          <BiUser />
-           </IconContext.Provider>
+          <IconContext.Provider value={{ color: 'white', size: '25px' }}>
+            <BiUser />
+          </IconContext.Provider>
           <i class={Style.fa_fa_caret_down}></i>
         </button>
         <div class={Style.dropdown_content}>
-          <a href="#" onClick={()=>OnLogout()}>Logout</a>
+          <a href="#" onClick={handleLogout}>Logout</a>
+
           {/*<a href="#">Link 2</a>
           <a href="#">Link 3</a>*/}
         </div>
