3e67862549f48def45d256628621f0e3369a3673
Diego Cuevas <ingdcuevas@gmail.com>
2022-09-21 22:53:26 -0500

    componentes pasarela de pago - estilos css

--- client/src/components/PasarelaDePago/FormularioContactoDelivery.jsx
+++ client/src/components/PasarelaDePago/FormularioContactoDelivery.jsx
@@ -1,15 +1,23 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import {
-  getCalendar,ChangeDeliveryInitial,ChangeDelivery
-} from '../../redux/actions'
+  getCalendar,
+  ChangeDeliveryInitial,
+  ChangeDelivery,
+} from "../../redux/actions";
 import { withRouter } from "react-router";
+import styles from "./FormularioContacto.module.css";
 
 export class FormularioContactoDelivery extends Component {
-
   constructor() {
     super();
-    this.state = { Dia: undefined, Hour: undefined ,adress:"",phone:"",reference:""};
+    this.state = {
+      Dia: undefined,
+      Hour: undefined,
+      adress: "",
+      phone: "",
+      reference: "",
+    };
   }
 
   componentDidMount() {
@@ -25,124 +33,140 @@ export class FormularioContactoDelivery extends Component {
     this.setState({ ...this.state, Hour: hour });
   }
 
-  handleChange(e,type){
+  handleChange(e, type) {
     this.setState({ ...this.state, [type]: e.target.value });
   }
 
-  Comprobaciones(){
- //Buscar Id
- const found = this.props.calendar.find(element => element.date==this.state.Dia && element.time==this.state.Hour)
- 
- this.props.ChangeDelivery({idDate:found,adress:this.state.adress 
-  ,phone:this.state.phone ,reference:this.state.reference})
-   
-  this.props.history.push("/MethodPay")
+  Comprobaciones() {
+    //Buscar Id
+    const found = this.props.calendar.find(
+      (element) =>
+        element.date == this.state.Dia && element.time == this.state.Hour
+    );
+
+    this.props.ChangeDelivery({
+      idDate: found,
+      adress: this.state.adress,
+      phone: this.state.phone,
+      reference: this.state.reference,
+    });
+
+    this.props.history.push("/MethodPay");
   }
 
   render() {
-    console.log(this.state.Dia)
-    console.log(this.props.calendar)
+    console.log(this.state.Dia);
+    console.log(this.props.calendar);
 
     var today = new Date();
-    var DaysMax = 14
+    var DaysMax = 14;
     var result = today.setDate(today.getDate() + DaysMax);
-    let result2 = new Date(result)
+    let result2 = new Date(result);
 
-    var Nuevoarray2 = []
+    var Nuevoarray2 = [];
     for (let index = 0; index < this.props.calendar.length; index++) {
-      const DATE = new Date(this.props.calendar[index].date)
-      if (DATE < result2)
-        Nuevoarray2.push(this.props.calendar[index])
+      const DATE = new Date(this.props.calendar[index].date);
+      if (DATE < result2) Nuevoarray2.push(this.props.calendar[index]);
     }
 
-    var Strings = []
+    var Strings = [];
     for (let index = 0; index < Nuevoarray2.length; index++) {
-      if (Strings.find(element => element === Nuevoarray2[index].date) === undefined)
-        Strings.push(Nuevoarray2[index].date)
+      if (
+        Strings.find((element) => element === Nuevoarray2[index].date) ===
+        undefined
+      )
+        Strings.push(Nuevoarray2[index].date);
     }
-    console.log(Strings)
-    console.log(Nuevoarray2)
+    console.log(Strings);
+    console.log(Nuevoarray2);
 
-    let NuevoArray3 = Nuevoarray2.filter(element => element.date === this.state.Dia)
-    console.log(NuevoArray3)
+    let NuevoArray3 = Nuevoarray2.filter(
+      (element) => element.date === this.state.Dia
+    );
+    console.log(NuevoArray3);
 
-    console.log(this.props.delivery)
+    console.log(this.props.delivery);
     return (
-      <div>
-        <label >Your Adress: </label>
-        <input
-          type="text"
-          id="Direccion"
-          autoComplete="off"
-          value={this.state.adress}
-          onChange={(e) => this.handleChange(e, "adress")}
-        />
-        <label  >Your Phone </label>
-        <input
-          type="text"
-          id="Phone"
-          autoComplete="off"
-          value={this.state.phone}
-          onChange={(e) => this.handleChange(e, "phone")}
-        />
-        <label  >Your Reference: </label>
-        <input
-          type="text"
-          id="Reference"
-          autoComplete="off"
-          value={this.state.reference}
-          onChange={(e) => this.handleChange(e, "reference")}
-        />
+      <div className={styles.containerFormulario}>
+        <div className={styles.flexContainerInput}>
+          <label>Your Adress: </label>
+          <input
+            type="text"
+            id="Direccion"
+            autoComplete="off"
+            value={this.state.adress}
+            onChange={(e) => this.handleChange(e, "adress")}
+          />
+          <label>Your Phone </label>
+          <input
+            type="text"
+            id="Phone"
+            autoComplete="off"
+            value={this.state.phone}
+            onChange={(e) => this.handleChange(e, "phone")}
+          />
+          <label>Your Reference: </label>
+          <input
+            type="text"
+            id="Reference"
+            autoComplete="off"
+            value={this.state.reference}
+            onChange={(e) => this.handleChange(e, "reference")}
+          />
+        </div>
 
-        <nav>
+        <nav className={styles.flexDayContainer}>
           <ul>
             {Strings.map((elemento) => {
-              let valor = elemento.substring(5, elemento.length)
+              let valor = elemento.substring(5, elemento.length);
               return (
                 <li
                   key={elemento}
-                  className="day-item"
-                  onClick={() =>
-                    this.changeDay(elemento)
-                  }
+                  onClick={() => this.changeDay(elemento)}
+                  className={elemento == this.state.Dia && styles.seleccionado}
                 >
                   {valor}
                 </li>
-              )
-            })
-            }
+              );
+            })}
           </ul>
         </nav>
-        {NuevoArray3 !== undefined && NuevoArray3.length > 0 ?
-          <div>
-            <nav>
+        {NuevoArray3 !== undefined && NuevoArray3.length > 0 ? (
+          <div className={styles.hourContainer}>
+            <nav className={styles.flexHourContainer}>
               <ul>
-                {
-                  NuevoArray3.map((elemento) => {
-                    return (
-                      <li
-                        key={elemento.time}
-                        className="hour-item"
-                        onClick={() =>
-                          this.changeHour(elemento.time)
-                        }
-                      >
-                        {elemento.time}
-                      </li>
-                    )
-                  }
-                  )
-                }
+                {NuevoArray3.map((elemento) => {
+                  return (
+                    <li
+                      key={elemento.time}
+                      onClick={() => this.changeHour(elemento.time)}
+                      className={
+                        elemento.time == this.state.Hour && styles.seleccionado
+                      }
+                    >
+                      {elemento.time}
+                    </li>
+                  );
+                })}
               </ul>
             </nav>
             {this.state.Hour === undefined && <label>CHOOSE HOUR</label>}
           </div>
-          :
-          "CHOOSE DAY"
-        }
-      {this.state.Dia!==undefined && this.state.Hour!==undefined && this.state.phone!=="" 
-      && this.state.reference!=="" && this.state.adress!=="" && 
-      <button onClick={()=>this.Comprobaciones()}>CONTINUE BUY</button>}
+        ) : (
+          <p>CHOOSE DAY</p>
+        )}
+        {this.state.Dia !== undefined &&
+          this.state.Hour !== undefined &&
+          this.state.phone !== "" &&
+          this.state.reference !== "" &&
+          this.state.adress !== "" && (
+            <button
+              className={styles.btnBuy}
+              onClick={() => this.Comprobaciones()}
+            >
+              CONTINUE BUY
+            </button>
+          )}
       </div>
     );
   }
@@ -154,19 +178,21 @@ function mapStateToProps(state) {
   return {
     calendar: state.calendar,
     delivery: state.delivery,
-  }
+  };
 }
 
 function mapDispatchToProps(dispatch) {
   //pasandole al componente la posibilidad como props de hacer un dispatch de la function getProducts
   return {
     getCalendar: (e) => dispatch(getCalendar(e)),
-    ChangeDeliveryInitial:()=>dispatch(ChangeDeliveryInitial()),
-    ChangeDelivery:(objectDelivery)=>dispatch(ChangeDelivery(objectDelivery))
-  }
+    ChangeDeliveryInitial: () => dispatch(ChangeDeliveryInitial()),
+    ChangeDelivery: (objectDelivery) =>
+      dispatch(ChangeDelivery(objectDelivery)),
+  };
 }
 
-
-
 //las propiedades del estado que quiero conectar //las acciones que quiero poder dispatchar
-export default connect(mapStateToProps, mapDispatchToProps)(FilterFormularioContactoDelivery);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(FilterFormularioContactoDelivery);
